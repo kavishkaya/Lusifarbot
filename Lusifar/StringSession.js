08e31208f7be2358d4fc25ad26f9e316b3eb2935
kavishkaya <91202334+kavishkaya@users.noreply.github.com>
2021-10-10 18:42:33 +0530

    Update StringSession.js

--- Lusifar/StringSession.js
+++ Lusifar/StringSession.js
@@ -1,8 +1,7 @@
-/* Copyright (C) 2020 Yusuf Usta.
 
+/* Copyright (C) 2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -13,7 +12,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('JULIE_SESSION' in process.env && string === undefined) {
+        if ('LUSIFAR_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -21,14 +20,14 @@ class StringSession {
             }
         }
         
-        var split = string.split(':::');
+        var split = string.split(';;;');
         if (split.length >= 2) {
             return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
         }
     }
 
     createStringSession(dict) {
-        return 'JulieMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'LUSIFAR;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
